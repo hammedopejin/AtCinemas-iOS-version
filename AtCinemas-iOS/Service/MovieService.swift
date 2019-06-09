affae31c05127245b6c4d0e79047a1c51825005e
Hammed Opejin <hammedopejin@yahoo.com>
2019-06-09 19:57:37 -0400

    feat: Create MovieService API calls

--- AtCinemas-iOS/Service/MovieService.swift
+++ AtCinemas-iOS/Service/MovieService.swift
@@ -8,3 +8,126 @@
 
 import Foundation
 
+enum MovieError: Error {
+    case badURLPath(String)
+}
+
+typealias MovieHandler =  ([Movie], Error?) -> Void
+typealias TrailerHandler = ([Trailer], Error?) -> Void
+typealias ReviewHandler = ([Review], Error?) -> Void
+
+let movieService = MovieService.shared
+
+final class MovieService {
+    
+    static let shared = MovieService()
+    private init() {}
+    
+    lazy var session: URLSession = {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 20
+        return URLSession(configuration: config)
+    }()
+    
+    //MARK: movies
+    func get(movies by: String, completion: @escaping MovieHandler) {
+        
+        var urlString = String()
+        
+        switch by {
+        case Constants.Keys.nowPlaying.rawValue:
+            urlString = MovieAPI.getNowPlayingUrl()
+            break
+        case Constants.Keys.popular.rawValue:
+            urlString = MovieAPI.getPopularUrl()
+            break
+        default:
+            urlString = MovieAPI.getTopRatedUrl()
+            break
+        }
+        
+        guard let finalURL = URL(string: urlString) else {
+            completion([], MovieError.badURLPath("Bad Url Path"))
+            return
+        }
+        
+        session.dataTask(with: finalURL) { (dat, _, err) in
+            
+            if let error = err {
+                completion([], error)
+                return
+            }
+            
+            if let data = dat {
+                do {
+                    let response = try JSONDecoder().decode(MovieResults.self, from: data)
+                    let movies = response.results.movies
+                    completion(movies, nil)
+                } catch let error {
+                    completion([], error)
+                }
+            }
+            
+        }
+        
+    }
+    
+    //MARK: trailers
+    func get(trailers with: String, completion: @escaping TrailerHandler) {
+        
+        let urlString = MovieAPI.getMovieTrailerUrl(with)
+        
+        guard let finalURL = URL(string: urlString) else {
+            completion([],MovieError.badURLPath("Bad Url Path"))
+            return
+        }
+        
+        session.dataTask(with: finalURL) { (dat, _, err) in
+            
+            if let error = err {
+                completion([], error)
+                return
+            }
+            
+            if let data = dat {
+                do {
+                    let response = try JSONDecoder().decode(TrailerResults.self, from: data)
+                    let trailers = response.results.trailers
+                    completion(trailers, nil)
+                } catch let error {
+                    completion([], error)
+                }
+            }
+        }
+    }
+    
+    //MARK: reviews
+    func get(reviews with: String, completion: @escaping ReviewHandler) {
+        
+        let urlString = MovieAPI.getMovieReviewUrl(with)
+        
+        guard let finalURL = URL(string: urlString) else {
+            completion([],MovieError.badURLPath("Bad Url Path"))
+            return
+        }
+        
+        session.dataTask(with: finalURL) { (dat, _, err) in
+            
+            if let error = err {
+                completion([], error)
+                return
+            }
+            
+            if let data = dat {
+                do {
+                    let response = try JSONDecoder().decode(ReviewResults.self, from: data)
+                    let reviews = response.results.reviews
+                    completion(reviews, nil)
+                } catch let error {
+                    completion([], error)
+                }
+            }
+        }
+    }
+    
+}
