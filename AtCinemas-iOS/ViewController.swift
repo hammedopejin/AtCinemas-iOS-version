51310179f5fc74ed49d958874d8b7d5b7cd86249
Hammed Opejin <hammedopejin@yahoo.com>
2019-06-08 23:47:34 -0400

    Initial Commit

---
+++ AtCinemas-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AtCinemas-iOS
+//
+//  Created by Hammed opejin on 6/8/19.
+//  Copyright © 2019 Hammed opejin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AtCinemas-iOSTests/AtCinemas_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  AtCinemas_iOSTests.swift
+//  AtCinemas-iOSTests
+//
+//  Created by Hammed opejin on 6/8/19.
+//  Copyright © 2019 Hammed opejin. All rights reserved.
+//
+
+import XCTest
+@testable import AtCinemas_iOS
+
+class AtCinemas_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
